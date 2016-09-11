b8ace9ff96b3cd020e2051efbb640a74ab1a50bf
AP-e <AP-e@users.noreply.github.com>
2016-09-12 00:22:13 +0100

    Implement prototype Shelf class to store Records
    Instead a list of Records with associated functions, a Shelf object
    provides a good way to store and access Record objects. Shelf should
    be initialisable with a variety of collection types (dataframe etc) but
    is currently only by `collection_folder`. Storage of records in dict
    is not yet for any purpose, and access has not been implemented in
    discogs_jockey module.

--- discogs_jockey/collection.py
+++ discogs_jockey/collection.py
@@ -1,29 +1,32 @@
 """ collection 
 
-Module for loading a collection from various sources to a shelf of Records
+Module for loading a collection from various sources to a shelf of Records.
+Note that discogs server requests should be rate limited.
 """
-from discogs_client import Release
+from discogs_client.models import Release, CollectionFolder
 
 class Record(object):
     """ A vinyl record, do not leave in direct sunlight. """
+    
     def __init__(self, release):
         if isinstance(release, Release):
             self._initialise_from_release()
         elif isinstance(release, dict):
             self._initialise_from_dict()
         else:
-            raise TypeError('Invalid release type: {}'.format(type(release))
+            raise TypeError('Invalid release type: {}'.format(type(release)))
     
     def _initialise_from_dict(self, details):
         """ Assign Record info from dict of release details."""
         
+        # Directly assign release details
         for attr in ['release_id', 'title', 'artists', 
                 'cat_nums', 'labels', 'year']:
-            setattr(self, attr) = details.pop(attr)
-        
-        if details:
+            setattr(self, attr, details.pop(attr))
+
+        if details: # ensure no superfluous info
             raise TypeError("Unused release details {}".format(
-                    list(details.keys()))
+                    list(details.keys())))
 
     def _initialise_from_release(self, release):
         """ Assign Record info from discogs_client.models.Release object."""
@@ -36,3 +39,29 @@ class Record(object):
         self.year = release.year
 
 
+class Shelf():
+    """ A shelf to hold Records. """
+
+    def __init__(self, collection):
+        """ Fill shelf with Records from either csv or discogs collection."""
+    
+        if isinstance(collection, CollectionFolder):
+            self._initialise_from_folder(collection)
+        elif isinstance(collection, ...): # from csv
+            NotImplemented
+
+    def _initialise_from_folder(self, folder):
+        """ Coerce discogs_client.models.CollectionFolder to Records."""
+        
+        good_formats = ['Vinyl',] # names of acceptable record formats
+
+        # Extract Release objects from folder
+        records = {} # ids as keys to merge copies
+        for item in folder.releases:
+            release = item.release
+            # Only store wax (i.e. exclude CDs, Tapes, MP3s etc)
+            formats = [format['name'] for format in release.formats]
+            if not set(formats).isdisjoint(good_formats):
+                records[release.id] = release
+        
+        self.records = records
