2ac31c791da827339fe7e86fc811a57a12ec6e6d
AP-e <AP-e@users.noreply.github.com>
2016-09-14 18:22:16 +0100

    Update script

--- Play_Discogs_Jockey.py
+++ Play_Discogs_Jockey.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
-from discogs_jockey.discogs_jockey import NoData, play_discogs_jockey 
+import sys
+from discogs_jockey.exceptions import NoData, StopPlaying
+from discogs_jockey.collection import Record, Crate, Shelf
+from discogs_jockey.interactor import TerminalInteractor
+from discogs_jockey.load import load_from_dir
+from discogs_jockey.game import Game
+
+# Initialise interactor
+io = TerminalInteractor()
 
 # Game parameters
-fpath = None # Which .csv file?
 crate_size = 5 # How many records can you choose from?
-replace = True # Should unplayed records be put back on the shelf?
+replace = False # Should unplayed records be put back on the shelf?
 
-try:
-    print("* * * Enjoy your set! * * *")
-    play_discogs_jockey(fpath, crate_size, replace)
-except NoData:
-    print("You need to put your Discogs collection .csv file in a folder called collection.")
+# Load collection to shelf
+fdir = "collection"
+df = load_from_dir(fdir)
+shelf = Shelf(df)
+
+# Play discogs jockey game
+game = Game(shelf, io, rules={'cap': crate_size, 'replace': replace})
+game.play_set()
+
+sys.exit()
