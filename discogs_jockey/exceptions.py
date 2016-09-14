19ae545b852d2eb93d54a52bac85f5d7bf4dceb6
AP-e <AP-e@users.noreply.github.com>
2016-09-14 14:02:52 +0100

    Add exception used to quit game

--- discogs_jockey/exceptions.py
+++ discogs_jockey/exceptions.py
@@ -6,3 +6,7 @@ Custom Exceptions for discogs_jockey.
 class NoData(OSError):
     """ .tv"""
     pass
+
+class StopPlaying(Exception):                                                   
+    """ Used to process a quit request."""                                      
+    pass
