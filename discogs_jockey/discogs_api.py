85e436806f10221f469a91619a872ef30ecdb637
AP-e <AP-e@users.noreply.github.com>
2016-09-15 23:09:11 +0100

    Make new module to handle interaction with Discogs API

---
+++ discogs_jockey/discogs_api.py
@@ -0,0 +1,10 @@
+""" discogs_api
+
+module for interacting with the discogs server via the discogs_client
+"""
+
+from discogs_client import Client
+from discogs_client.exceptions import HTTPError
+import os
+
+
