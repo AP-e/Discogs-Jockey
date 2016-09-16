0a733a6cc76cf12b65d461a9e6def2d88aa9c130
AP-e <AP-e@users.noreply.github.com>
2016-09-16 02:33:21 +0100

    Update play script

--- Play_Discogs_Jockey.py
+++ Play_Discogs_Jockey.py
@@ -6,6 +6,7 @@ from discogs_jockey.collection import Record, Crate, Shelf
 from discogs_jockey.interactor import TerminalInteractor
 from discogs_jockey.load import load_from_dir
 from discogs_jockey.game import Game
+from discogs_jockey.discogs_api import get_collection_from_discogs
 
 # Initialise interactor
 io = TerminalInteractor()
@@ -14,13 +15,33 @@ io = TerminalInteractor()
 crate_size = 5 # How many records can you choose from?
 replace = False # Should unplayed records be put back on the shelf?
 
-# Load collection to shelf
-fdir = "collection"
-df = load_from_dir(fdir)
-shelf = Shelf(df)
+def get_collection_offline():
+    # Load csv collection to shelf
+    fdir = "collection"
+    df = load_from_dir(fdir)
+    return df
+
+def get_collection_online():
+    # Get a collection
+    try:
+        collection = get_collection_from_discogs(io)
+    except HTTPError: # it's not possible to get anything other than code 400
+        print('Authorisation failed, try again?') # or upload collection, etc    
+    return collection
+
+collection = get_collection_online()
+if not collection.releases:
+    print("You don't have any records in your collection!")
+    print("Quitting...")
+    sys.exit()
+
+# Load up shelf
+shelf = Shelf(collection)
+print("{} records loaded from your collection to the shelf, let's play!".format(len(collection.releases)))
+game = Game(shelf, TerminalInteractor())
+game.play_set() 
 
 # Play discogs jockey game
 game = Game(shelf, io, rules={'cap': crate_size, 'replace': replace})
 game.play_set()
-
 sys.exit()
